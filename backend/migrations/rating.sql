6fb0d468b9613e8f8c21c74123bf4831bb63a043
Anime-Girl <51544746+Anime-Girl@users.noreply.github.com>
2022-12-06 01:50:36 +0300

    Update rating.sql

--- backend/migrations/rating.sql
+++ backend/migrations/rating.sql
@@ -1,20 +1,15 @@
-CREATE review_type AS ENUM (
-    'client_side',
-    'master_side'
-)
-
 CREATE TABLE reviews (
-    -- unique id of the review
-    review_id BIGSERIAL NOT NULL,
-    -- unique task id
+    -- task id
     task_id BIGSERIAL NOT NULL,
     -- type of the review, mainly for direction 
     -- of the feedback
-    type review_type NOT NULL,
+    type SMALLINT NOT NULL,
     -- id of the master assigned to the task
     master_id BIGSERIAL NOT NULL,
     -- id of the client who requested the task
     client_id BIGSERIAL NOT NULL,
     -- normalized_score, all scores are topped at 1
-    normalized_score REAL NOT NULL,
-)
+    normalized_score REAL NOT NULL
+);
+
+CREATE UNIQUE INDEX reviews_task_id_type ON reviews (task_id, type);
