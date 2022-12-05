f1487d72499b88f6fb925dcb2f84a9073de13820
Anime-Girl <51544746+Anime-Girl@users.noreply.github.com>
2022-12-06 02:03:37 +0300

    Update payments.sql

--- backend/migrations/payments.sql
+++ backend/migrations/payments.sql
@@ -9,10 +9,8 @@ CREATE TABLE payment_statuses {
     to_metadata TEXT NOT NULL,
     -- transation status
     status SMALLINT NOT NULL,
-    
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-
-    -- use task_id as the primary key for lookups
-    CONSTRAINT payment_statuses_pk PRIMARY KEY (task_id)
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 };
+
+CREATE UNIQUE INDEX payment_statuses_task_id_unique_idx ON payment_statuses (task_id);
