1707745a3047e15483e66185b0024633703ca71e
Anime-Girl <51544746+Anime-Girl@users.noreply.github.com>
2022-12-06 02:07:14 +0300

    Update rating.sql

--- backend/migrations/rating.sql
+++ backend/migrations/rating.sql
@@ -1,13 +1,13 @@
 CREATE TABLE reviews (
     -- task id
-    task_id BIGSERIAL NOT NULL,
+    task_id BIGINT NOT NULL,
     -- type of the review, mainly for direction 
     -- of the feedback
     type SMALLINT NOT NULL,
     -- id of the master assigned to the task
-    master_id BIGSERIAL NOT NULL,
+    master_id BIGINT NOT NULL,
     -- id of the client who requested the task
-    client_id BIGSERIAL NOT NULL,
+    client_id BIGINT NOT NULL,
     -- normalized_score, all scores are topped at 1
     normalized_score REAL NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
