0d7e55dbeec90ff6a127261591053c2e14abd783
Anime-Girl <51544746+Anime-Girl@users.noreply.github.com>
2022-12-06 02:05:05 +0300

    Update status.sql

--- backend/migrations/status.sql
+++ backend/migrations/status.sql
@@ -2,6 +2,7 @@ CREATE TABLE status {
     task_id BIGINT NOT NULL,
     status SMALLINT NOT NULL, 
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    CONSTRAINT status_pk PRIMARY KEY (task_id)
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 };
+
+CREATE UNIQUE INDEX status_task_id_unique_idx ON master (task_id);
