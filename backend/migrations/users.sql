a634dae2477cb8d8b416c94ce2505181169cbec2
koalaannette <koalaannette@yandex.ru>
2022-12-06 04:59:04 +0300

    Create users.sql

---
+++ backend/migrations/users.sql
@@ -0,0 +1,20 @@
+CREATE TYPE user_role AS ENUM ('master', 'client');
+
+CREATE TYPE user_status AS ENUM ('pending', 'accepted', 'blocked');
+
+CREATE TABLE reviews (
+    -- unique id of the user
+    user_id BIGSERIAL NOT NULL,
+    -- name, surname of the user
+    username TEXT NOT NULL,,
+    -- user's login
+    login TEXT NOT NULL,
+    -- user's password
+    password TEXT NOT NULL,
+    -- user's role
+    role user_role NOT NULL,
+    -- user's after registration
+    status user_status NOT NULL,
+    -- creation timestamp
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+)
